9494c81754eb09ab943570563360a9c6189b1f02
venel._ <erazzorlinx@gmail.com>
2026-02-17 13:00:26 +0100

    mobile/feat: Added itim font

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,24 @@
 import '../global.css';
 
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        Itim: require('../assets/fonts/Itim-Regular.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View className="flex-1 items-center justify-center bg-background">
+                <ActivityIndicator size="large" color="primary" />
+            </View>
+        );
+    }
+
     return (
         <SafeAreaProvider>
             <StatusBar style="light" />
